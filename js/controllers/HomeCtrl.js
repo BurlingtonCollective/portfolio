26627e9f60760ea2eb4f4def9fd09aca996202dd
Zachary Lincoln <zach@burlingtoncollective.com>
2014-06-24 21:21:00 -0400

    basic layout

--- js/controllers/HomeCtrl.js
+++ js/controllers/HomeCtrl.js
@@ -1,17 +1,22 @@
 controllers.controller('HomeCtrl', ['$scope', 'contactService', function($scope, contactService){
+	
 	$scope.sendContact = function(){
-		console.log($scope.contactForm);
 		if($scope.contactForm.honeypot){
 			//spam detection
-			console.log($scope.contactForm.honeypot);
+			//show error
 		}else{
 			var cleanObj = {
 				name: $scope.contactForm.name,
 				email: $scope.contactForm.email,
 				message: $scope.contactForm.message
 			};
-			contactService.pushSimpleForm(cleanObj);
+			var result = contactService.pushSimpleForm(cleanObj);
+			if(result.data.success){
+				//showmessage
+			}else{
+				//show error
+			}
 		}
-
 	}
+	
 }]);
--- js/services/contactService.js
+++ js/services/contactService.js
@@ -1,19 +1,20 @@
 services.service('contactService', ['$http', '$q', function($http, $q){
+
 	var self = this;
+
 	this.simpleFormToken = 'd7d6a0a7ea62ea0febb7da1e74686972';
+
 	this.pushSimpleForm = function(formObj){
 		console.log(formObj);
 		var deferred = $q.defer();
 		$http.jsonp('http://getsimpleform.com/messages/ajax?form_api_token='+self.simpleFormToken+'&callback=JSON_CALLBACK', {
 			params: formObj
 		}).then(function(result){
-			console.log('submitted');
-			console.log(result);
+			deferred.resolve(result);
 		}, function(response){
-			console.log(response);
-			console.log('no-go');
 			deferred.reject(response);
 		});
 		return deferred.promise;
 	}
+	
 }]);
